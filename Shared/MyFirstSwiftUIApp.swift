0aae83221268a97ce22faa04a30bd882c7ad83d6
seyma.yilmaz <seyma.yilmaz@trendyol.com>
2020-12-18 09:48:54 +0300

    Initial Commit

---
+++ Shared/MyFirstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftUIApp.swift
+//  Shared
+//
+//  Created by Şeyma Yılmaz on 18.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
